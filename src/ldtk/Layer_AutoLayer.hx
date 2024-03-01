80946fb95994b9e3abd62c4f2ef532dcb1e44988
mufty <gubriansky@sernato.sk>
2024-03-01 12:25:39 +0100

    tileset can be rewritten

--- src/ldtk/Layer_AutoLayer.hx
+++ src/ldtk/Layer_AutoLayer.hx
@@ -37,7 +37,7 @@ class Layer_AutoLayer extends ldtk.Layer {
 		inline function get_untypedTileset() return untypedProject._untypedTilesets.get(tilesetUid);
 
 	/** Tileset UID **/
-	public var tilesetUid(default,null) : Int;
+	public var tilesetUid : Int;
 
 
 	public function new(p,json) {
